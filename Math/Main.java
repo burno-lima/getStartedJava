3b8db594b28d0c89b712f95df36a87b08a3c9034
Bruno Gustavo <bruno.lima@nappsolutions.com>
2023-06-13 15:40:53 -0300

    Java Math

---
+++ Math/Main.java
@@ -0,0 +1,9 @@
+public class Main{
+  public static void main(String[] args){
+    System.out.println(Math.max(5, 10));
+
+    System.out.println(Math.min(5, 10));
+
+    System.out.println(Math.sqrt(64));
+  }
+}
