4234b946a0f0ba36c688513d111712ce9e48cc23
Bruno Gustavo <bruno.lima@nappsolutions.com>
2023-06-13 15:11:28 -0300

    Java Type Casting

---
+++ TypeCasting/Main.java
@@ -0,0 +1,15 @@
+public class Main{
+  public static void main(String[] args){
+    int myInt = 9;
+    double myDouble = myInt;
+
+    System.out.println(myInt);
+    System.out.println(myDouble);
+
+    double myDoubles = 9.78d;
+    int myInts = (int) myDoubles;
+
+    System.out.println(myDoubles);
+    System.out.println(myInts);
+  }
+}
