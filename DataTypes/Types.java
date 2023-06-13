c4918b4225c9c961a49b674df1df649250e1f5fd
Bruno Gustavo <bruno.lima@nappsolutions.com>
2023-06-13 15:03:44 -0300

    Java Data Types

---
+++ DataTypes/Types.java
@@ -0,0 +1,31 @@
+public class Types{
+  public static void main(String[] args){
+  
+  // Numbers
+  // byte
+  byte myBytes = 100;
+  System.out.println(myBytes);
+
+  // short
+  short myShort = 5000;
+  System.out.println(myShort);
+
+  // long
+  long myLong = 15000000000L;
+  System.out.println(myLong);
+
+  // double
+  double myDouble = 5.75d;
+  System.out.println(myDouble);
+
+  // Boolean
+  boolean isJavaFun = true;
+  boolean isFishTasty = false;
+  System.out.println(isJavaFun);
+  System.out.println(isFishTasty);
+
+  // Characters
+  char myGrade = 'B';
+  System.out.println(myGrade);
+  }
+}
