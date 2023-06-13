7b4632f2891e12a45818209456fe69de80038615
Bruno Gustavo <bruno.lima@nappsolutions.com>
2023-06-13 14:16:54 -0300

    Java Variables

---
+++ Variables/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+  public static void main(String[] args) {
+    String name = "Burno";
+    System.out.println(name);
+
+    int age;
+    age = 23;
+    System.out.println(age);
+
+    float numFloat = 5.99f;
+    System.out.println(numFloat);
+  }
+}
---
+++ Variables/Name.java
@@ -0,0 +1,10 @@
+public class Name {
+  public static void main(String[] args){
+    String firstName = "Burno ";
+    String lastName = "Lima";
+
+    String fullName = firstName + lastName;
+
+    System.out.println(fullName);
+  }
+} 
