ffaadc25f14500b63132bf687dafb312a3b43a36
Bruno Gustavo <bruno.lima@nappsolutions.com>
2023-06-13 17:17:17 -0300

    Java Loop Arrays, MultiArrays

---
+++ Arrays/MultiArrays.java
@@ -0,0 +1,11 @@
+public class MultiArrays{
+  public static void main(String[] args){
+
+    int[][] myNumbers = {{1,2,3,4}, {5,6,7}};
+
+    System.out.println(myNumbers[1][2]);
+
+    myNumbers[1][2] = 9;
+    System.out.println(myNumbers[1][2]);
+  }
+}
