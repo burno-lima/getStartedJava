6c8a4698932ce844a9ac086691e528828ba99a6a
Bruno Gustavo <bruno.lima@nappsolutions.com>
2023-06-13 17:09:58 -0300

    Java Arrays

---
+++ Arrays/Main.java
@@ -0,0 +1,18 @@
+public class Main{
+  public static void main(String[] args){
+
+    // Arrays Strings
+    String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
+
+    // Arrays Numbers
+    int[] myNum = {10, 20, 30, 40};
+
+    System.out.println(cars[0]);
+
+    cars[0] = "Opel";
+
+    System.out.println(cars[0]);
+
+    System.out.println(cars.length);
+  }
+}
