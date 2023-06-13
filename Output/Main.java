76b420d68289cd752019f7fac76204075651c1b9
Bruno Gustavo <bruno.lima@nappsolutions.com>
2023-06-13 14:00:52 -0300

    Java Output

---
+++ Output/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.print("Hello World!");
+    System.out.print("I am learning Java.");
+    System.out.print(10 * 2);
+  }
+}
