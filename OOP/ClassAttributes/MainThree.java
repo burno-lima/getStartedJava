788e7e43f1063d0f2f147d45df0aa7de3c405f68
Bruno Lima <brunoluislima19@gmail.com>
2023-07-10 23:49:23 -0300

    Java Class Attributes

---
+++ OOP/ClassAttributes/MainThree.java
@@ -0,0 +1,9 @@
+public class MainThree {
+  int x = 10;
+
+  public static void main(String[] args){
+    Main myobj = new Main();
+    myobj.x = 25;
+    System.out.println(myobj.x);
+  }
+}
---
+++ OOP/ClassAttributes/MultAttri.java
@@ -0,0 +1,11 @@
+public class MultAttri {
+  String FNAME = "Burno";
+  String LNAME = "Lima";
+  int AGE = 23;
+
+  public static void main(String[] args) {
+    Main myObj = new Main();
+    System.out.println("Name: ", myObj.FNAME, " " + myObj.LNAME);
+    System.out.println("Age: " + myObj.AGE);
+  }
+}
