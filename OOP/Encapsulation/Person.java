db252bf0050d3173dc1e50ed8e95db9413dcc019
Bruno Lima <brunoluislima19@gmail.com>
2023-07-13 22:41:26 -0300

    Java Encapsulation

---
+++ OOP/Encapsulation/Person.java
@@ -0,0 +1,12 @@
+public class Person{
+  private String name; // private = retricted access
+  
+  // Getter
+  public String getName(){
+    return name;
+  }
+
+  public void setName(String newName){
+    this.name = newName;
+  }
+}
